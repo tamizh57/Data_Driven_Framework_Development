52bcf741c1ce9b6c7e511b463503db721715271d
Tamizh Selvan S <Tamizh Selvan S@DESKTOP-C88EB95>
2021-10-06 08:11:11 -0700

    4th commit

--- Jenkins_and_Git/src/Test_Runner.java
+++ Jenkins_and_Git/src/Test_Runner.java
@@ -5,7 +5,7 @@ import org.junit.runner.notification.Failure;
 public class Test_Runner {
 	public static void main(String args[])
 	{
-		//comment added to test jenkin's poll scm
+		//comment added to test jenkins poll scm
 		Result result = JUnitCore.runClasses(Test_Prime.class);
 		for(Failure failure:result.getFailures())
 		{
